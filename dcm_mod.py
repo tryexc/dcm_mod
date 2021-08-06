c16ab3c2126961a22f923454a1f32f70f9187894
tryexc <88328518+tryexc@users.noreply.github.com>
2021-08-06 10:34:01 +0200

    Update dcm_mod.py

--- dcm_mod.py
+++ dcm_mod.py
@@ -3,7 +3,7 @@ Created on Tue Aug  3 11:15:21 2020
 
 @author: tryexc
 
-needs Pydicom
+Needs Pydicom: https://pydicom.github.io/
 """
 import shutil
 import time
@@ -24,7 +24,6 @@ errorlog = '.../copyErrorLog.txt' # set path to error logfile
 def list_files(directory, extension):
     return (f for f in listdir(directory) if f.endswith('.' + extension))
 
-
 for patDir in os.listdir(p):
     for seqDir in os.listdir(p+'/'+patDir):
         files = list_files( p+'/'+patDir + '/' + seqDir, "dcm")
@@ -43,7 +42,6 @@ for patDir in os.listdir(p):
                 dataset.data_element('StudyDescription').value = StudDes
                 dataset.data_element('PerformingPhysicianName').value = '' # removed here
                 
-
                 dataset.save_as(dst + '/' + f)
                 print('Save: '+dst + '/' + f)
                 
@@ -52,6 +50,7 @@ for patDir in os.listdir(p):
                 log_file = open(errorlog, 'a')
                 log_file.write(src + '\n')
                 log_file.close()
+                
 print('remove orig. data....')
 #shutil.rmtree(p) #  if needed!
 print('done....')
